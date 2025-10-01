ffb70246068340bc657b0a7b8416631a81feaada
Arco Estrategias <arcoestrategias@outlook.com>
2025-10-01 00:58:01 -0500

    Arreglo de docker compose de qa

--- src/common/helpers/url.helper.ts
+++ src/common/helpers/url.helper.ts
@@ -1,5 +1,5 @@
 export const PUBLIC_BASE_URL = (
-  process.env.PUBLIC_BASE_URL ?? 'http://localhost:3500'
+  process.env.PUBLIC_BASE_URL ?? 'http://localhost:3000'
 ).replace(/\/$/, '');
 
 export const buildUrl = (path: string) =>
